34db75bc7a13961a5e8c4354316e3c399b20df66
Vbeni <benivillafana@gmail.com>
2023-02-01 13:38:12 -0500

    <updated solution.txt>

--- act1.sh
+++ act1.sh
@@ -9,7 +9,8 @@ touch star_wars/empire/darth_vader.txt
 # 4. Use the force (or your echo) to add the text "...heavy breathing..." to the darth_vader.txt file (Don’t remember how to do this? Use the ‘other force’, known as Google!)
 echo "...heavy breathing..." >> star_wars/empire/darth_vader.txt 
 # 5. Inside the empire directory, create a file called emperor_palpatine.txt
-
+touch star_wars/empire/emperor_palpatine.txt
 # 6. Inside the empire directory, create a directory called death_star
-
+mkdir star_wars/empire/death_star
 # 7. Move darth_vader.txt into the death_star
+mv star_wars/rebellion/darth_vader.txt star_wars/empire/death_star #tab autocompleted mv to 'Move' and I wanted to try it out. I guess it was refrencing "Move" since it was typed out in the instructions before
--- act2.sh
+++ act2.sh
@@ -1,13 +1,14 @@
 # 1. Inside the `star_wars/rebellion` directory, (IN ONE COMMAND) create a file called princess_leia.txt with the text "Help me, Obi-Wan…You’re my only hope."
-
+touch princess_leia.txt && echo "Help me, Obi-Wan…You’re my only hope." >> princess_leia.txt 
 # 2. Create a file called obi_wan.txt in star_wars/rebellion
-
+touch star_wars/rebellion/obi_wan.txt
 # 3. Create a file in star_wars/rebellion called luke_skywalker.txt
-
+touch star_wars/rebellion/luke_skywalker.txt
 # 4. Create a directory in star_wars/rebellion called millenium_falcon
-
+mkdir star_wars/rebellion/millenium_falcon
 # 5. Inside the millenium_falcon, create two files: han_solo.txt and chewbacca.txt
-
+touch star_wars/rebellion/millenium_falcon/han_solo.txt chewbacca.txt #tested out && here to see if i'd have to specify 'touch' twice...i did indeed need to, better without &&
 # 6. Move luke_skywalker, obi_wan, and princess_leia into the millenium_falcon, respectively.
-
+mv star_wars/rebellion/luke_skywalker.txt obi_wan.txt princess_leia.txt -t star_wars/rebellion/millenium_falcon
 # 7. Move the millenium_falcon into the death_star.
+mv millenium_falcon/ ../empire/death_star
